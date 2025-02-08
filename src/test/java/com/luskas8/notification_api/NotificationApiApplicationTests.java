b6ffd01f6e471078ed5b0d4cf97ea17895c56b9f
luskas8 <lucas.anjos@franq.com.br>
2025-02-08 12:11:02 -0300

    chore: initial commit

---
+++ src/test/java/com/luskas8/notification_api/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luskas8.notification_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
